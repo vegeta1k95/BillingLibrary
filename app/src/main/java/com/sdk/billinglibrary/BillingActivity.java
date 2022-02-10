1496b5f2eea3a8a89bc4fdfde90f9151ac606f5a
Serhii Dylda <tunerzinc@gmail.com>
2022-02-10 23:45:45 +0100

    Add color attr for button

--- app/src/main/java/com/sdk/billinglibrary/BillingActivity.java
+++ app/src/main/java/com/sdk/billinglibrary/BillingActivity.java
@@ -2,6 +2,7 @@ package com.sdk.billinglibrary;
 
 import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
@@ -86,7 +87,9 @@ public class BillingActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_billing);
 
-        //Window window = getWindow();
+        Window window = getWindow();
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        window.setStatusBarColor(Color.TRANSPARENT);
         //window.setStatusBarColor(MaterialColors.getColor(this, R.attr.billing_background_color, R.attr.colorAccent));
 
         featuresContainer = findViewById(R.id.features_container);
